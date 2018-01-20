829d9a3fa1e90c96ff27171ea17646c18dde0e5d
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2018-01-20 22:53:05 +0100

    nya bakgrundsbilder
    lade in några medan gustav fixar sin treeline

--- game_rooms.py
+++ game_rooms.py
@@ -90,7 +90,7 @@ class Room1(Room):
         self.level_height = 150
         self.level_low = 0
         
-        self.background.add(Background(0,0,800,600,"background-1.png"))
+        self.background.add(Background(0,0,800,600,"moone_ver_3.png"))
 
         obstacles = [[-20,570,1290,10,green],
                     [100,-200,20,726,green],
@@ -147,8 +147,8 @@ class Room2(Room):
         self.level_height = 0
         self.level_low = 0
 
-        self.background.add(Background(0,0,800,600,"background-1.png"))
-        self.background_layer.add(Background(0,0,1300,600,"treeline.png"))
+        self.background.add(Background(0,0,800,600,"moone_ver_3.png"))
+        self.background_layer.add(Background(0,0,1300,600,"treeline_01.png"))
 
         obstacles = [[-20,570,496,10,green],
                     [550,570,770,10,green],
